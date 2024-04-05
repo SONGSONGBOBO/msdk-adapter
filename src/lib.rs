75799dd2fb9a8c0ab9038e9809e6b160d21f79d4
SONGSONGBOBO <1127568060@qq.com>
2024-04-06 00:07:46 +0800

    [rust] change lib loading

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,25 @@
 use std::time::Duration;
-use libc::c_char;
 use anyhow::{Error, Result};
-
-#[link(name = "msdk", kind = "static")]
-extern "C" {
-    fn M_Open(arg: u64) -> u64;
-    fn M_KeyPress(hdl: u64, arg1: i64, arg2: i64) -> i32;
-    fn M_KeyInputString(hdl: u64, buf: *const c_char, len: u64) -> i64;
-    fn M_GetCurrMousePos2(x: *mut i64, y: *mut i64) -> i64;
-    fn M_Close(hdl: u64) -> i32;
-}
-
-pub enum MsdkResultCode {
-    SUCCESS(i32)
-}
-
-impl MsdkResultCode {
-    pub fn is_success(code: i32) -> bool {
-        return code == 0;
-    }
-}
+use libloading::{Library, Symbol};
 
 const SUCCESS: i32 = 0; 
 
 pub struct MsdkAdapter {
     port: u64,
-    handler: u64
+    handler: u64,
+    lib: Library
 }
 
 impl MsdkAdapter {
     // port num, start from 1
     pub fn new(port: u64) -> Result<MsdkAdapter> {
         unsafe {
+            let lib = Library::new("msdk.dll")?;
+            let open_func: Symbol<extern "C" fn(u64) -> u64> = lib.get(b"M_Open")?;
             let res = MsdkAdapter {
                 port,
-                handler: M_Open(port)
+                handler: open_func(port),
+                lib
             };
             std::thread::sleep(Duration::from_secs(3));
             Ok(res)
@@ -44,7 +29,7 @@ impl MsdkAdapter {
 
 fn check_result(result: i32) -> Result<i32> {
     match result {
-        0 => Ok(result),
+        SUCCESS => Ok(result),
         _ => {
             Err(Error::msg(format!("Check res failed, result: {}.", result)))
         }
@@ -60,8 +45,9 @@ pub trait MsdkOperation {
 impl MsdkOperation for MsdkAdapter {
     fn open(&mut self, port_num: u64) -> Result<u64> {
         unsafe {
-            self.close().expect(format!("Open port{} fail.", port_num));
-            let handler = M_Open(self.port);
+            self.close().expect(&format!("Open port{} fail.", port_num));
+            let open_func: Symbol<extern "C" fn(u64) -> u64> = self.lib.get(b"M_Open")?;
+            let handler = open_func(self.port);
             self.handler = handler;
             self.port = port_num;
             Ok(handler)
@@ -70,7 +56,8 @@ impl MsdkOperation for MsdkAdapter {
 
     fn close(&self) -> Result<i32> {
         unsafe {
-            let res = M_Close(self.handler);
+            let func: Symbol<extern "C" fn(u64) -> i32> = self.lib.get(b"M_Close").unwrap();
+            let res = func(self.handler);
             check_result(res)
         }
     }
